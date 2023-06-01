16c464dbf4b7b1dee748ada89da5c2dc82a5bbbc
Gerardo <geragrungeeb@gmail.com>
2023-06-01 16:40:58 -0300

    Se sube estilos navBar

--- src/components/Nav/navBar.jsx
+++ src/components/Nav/navBar.jsx
@@ -1,19 +1,21 @@
 import SearchBar from "../SearchBar/SearchBar";
 import style from "./navBar.module.css"
 import { Link } from "react-router-dom";
+import RyMlogo from '../img/RyMlogo.png'
 
 export default function Nav (props) {
     return (
         <nav className={style.contenedor}>
-            <div>
-                <Link to={'/cards'}>
-                    <span>Home</span>
+            <img className={style.img} src={RyMlogo} alt='Logo Rick and Morty'/>
+            <div className={style.navBtn}>
+                <Link className={style.link} to={'/cards'}>
+                    <span className={style.Btn}>Home</span>
                 </Link>
-                <Link to={'/about'}>
-                    <span>About</span>
+                <Link className={style.link} to={'/about'}>
+                    <span className={style.Btn}>About</span>
                 </Link>
-                <Link to={'/favorite'}>
-                    <span>Favorite</span>
+                <Link className={style.link} to={'/favorite'}>
+                    <span className={style.Btn}>Favorite</span>
                 </Link>
             </div>
             <SearchBar onSearch = {props.onSearch} />
